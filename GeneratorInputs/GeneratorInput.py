5472873a47d2d32ab92437c19b1d721a4f71eda1
Arthur Stevam <arthur.stevam.costa@ccc.ufcg.edu.br>
2019-10-16 19:02:20 -0300

    Gerador de Inputs

---
+++ GeneratorInputs/GeneratorInput.py
@@ -0,0 +1,35 @@
+from StringType import StringType
+from IntegerType import IntegerType
+from BooleanType import BooleanType
+from FloatType import FloatType
+
+class GeneratorInput:
+    inputs = []    
+    def __init__(self, inputs):
+        self.file = open('.test', 'r').readlines()
+        self.inputscreated = inputs
+
+    def generate_inputs(self):
+        for line in self.file:
+            self.inputs.append(line.split())
+        
+        for input_ in self.inputs:
+            if input_[0].lower() == 'int' or input_[0].lower() == 'float':
+                input_[1] = input_[1].split('<>')
+
+            elif input_[0].lower() == 'str':
+                input_[2] = input_[2].split('..')
+       
+        for input_ in self.inputs:
+
+            if input_[0].lower() == 'int':
+                variable = IntegerType(int(input_[1][0]), int(input_[1][1])).getInteger()
+            elif input_[0].lower() == 'float':
+                variable = FloatType(float(input_[1][0]), float(input_[1][1]), 2).getFloat()
+            elif input_[0].lower() == 'bool':
+                variable = BooleanType().getBoolean()
+            elif input_[0].lower() == 'str':
+                variable = StringType(int(input_[1]), input_[2][0], input_[2][1]).getString()
+                
+            self.inputscreated.append(variable)
+
---
+++ GeneratorInputs/test.py
@@ -0,0 +1,13 @@
+#Arthur Stevam 
+#UFCG - Ciências da Computação - 2019.2
+#
+
+from GeneratorInput import GeneratorInput
+
+inputs = []
+
+gerador = GeneratorInput(inputs)
+
+gerador.generate_inputs()
+
+print(inputs)
