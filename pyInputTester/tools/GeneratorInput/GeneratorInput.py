724ef0fb6809c63680588f366d1194619934540f
davig-sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2019-10-27 17:31:04 -0300

    Minor change

--- pyInputTester/tools/GeneratorInput/GeneratorInput.py
+++ pyInputTester/tools/GeneratorInput/GeneratorInput.py
@@ -4,6 +4,7 @@ class GeneratorInput:
     inputs = []
 
     def __init__(self, number_of_cases, path_to_file='.test'):
+        self.number_of_cases = number_of_cases
         self.__file = open(path_to_file, 'r').readlines()
         self.inputs_created = []
 
@@ -37,28 +38,16 @@ class GeneratorInput:
 
     def generate_inputs(self):
         self.search_variables()
+
+        for case in range(self.number_of_cases):
+            is_input = False
+            for line in self.__file:
+                line = line.strip()
+                if line == 'input:':
+                    is_input = True
+                elif is_input:
+                    keys = self.__get_statements(line)
+                    
+
+
         return self.inputs_created
-        # for line in self._file:
-        #     self.inputs.append(line.split())
-        
-        # for _input in self.inputs:
-        #     if _input[0].lower() == 'int' or _input[0].lower() == 'float':
-        #         _input[1] = _input[1].split('<>')
-
-        #     elif _input[0].lower() == 'str':
-        #         _input[2] = _input[2].split('...')
-       
-        # for _input in self.inputs:
-
-        #     if _input[0].lower() == 'int':
-        #         variable = IntegerType(int(_input[1][0]), int(_input[1][1])).get_integer()
-        #     elif _input[0].lower() == 'float':
-        #         variable = FloatType(float(_input[1][0]), float(_input[1][1]), 2).get_float()
-        #     elif _input[0].lower() == 'bool':
-        #         variable = BooleanType().get_boolean()
-        #     elif _input[0].lower() == 'str':
-        #         variable = StringType(int(_input[1]), _input[2][0], _input[2][1]).get_string()
-                
-        #     self.inputs_created.append(variable)
-        # return self.inputs_created
-
