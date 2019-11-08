6653188c879bf35df101368e2e57da78a72bf96b
Arthur Stevam <arthur.stevam.costa@ccc.ufcg.edu.br>
2019-11-08 17:56:38 -0300

    Methods __statements__ implemented

--- pyInputTester/tools/Types/FloatType.py
+++ pyInputTester/tools/Types/FloatType.py
@@ -2,14 +2,12 @@ import random
 
 class FloatType:
 
-    def __init__(self, minimum, maximum, decimal):
-        self.minimum = minimum
-        self.maximum = maximum
-        self.decimal = decimal
-
-    def get_float(self):
+    def __init__(self):
+        pass
+    def get_float(self, minimum, maximum, decimal):
+        minimum, maximum, decimal = float(minimum), float(maximum), int(decimal)
         while True:
-            float_ = (random.random()) * self.maximum
-            if float_ >= self.minimum: break
+            float_ = (random.random()) * maximum
+            if float_ >= minimum: break
     
-        return round(float_, self.decimal)
+        return round(float_, decimal)
--- pyInputTester/tools/__statements__.py
+++ pyInputTester/tools/__statements__.py
@@ -1,5 +1,5 @@
 from pyInputTester.tools.Types import *
-# from Types import *
+#from Types import *
 
 def __split(string, line):
     att = ''
@@ -15,7 +15,7 @@ def __split(string, line):
                 j += 1
             config.append(att)
         att = ''
-    if string != 'float':
+    if string != 'bool':
         string += '('
         config = [att.strip(string) for att in config]
         config = [att.strip(')') for att in config]
@@ -41,16 +41,35 @@ def get_str(line):
         inputs[1] = inputs[1].strip()      
         inputs[1] = inputs[1].split('...')
         param = string.get_string(int(inputs[0]), inputs[1][0], inputs[1][1])
-        old_string = 'str(' + inputs[0] + ', ' + inputs[1][0] + '...' + inputs[1][1] + ')'        
+        old_string = 'str(' + inputs[0] + ', ' + inputs[1][0] + '...' + inputs[1][1] + ')' 
         line = line.replace(old_string, param)
 
     return line
 
 def get_float(line):
-    pass
+    __config = __split('float', line)
+    __config = [att.split(',') for att in __config]
+    for __conf in __config:
+        __conf[0] = __conf[0].split('<>')
+        __conf[1] = __conf[1].strip()
+    
+    float_type = FloatType()
+
+    for __conf in __config:
+        param = float_type.get_float(__conf[0][0], __conf[0][1], __conf[1])
+        old_string = 'float(' + __conf[0][0] + '<>' + __conf[0][1] + ', ' + __conf[1] + ')'
+        line = line.replace(old_string, str(param))
+    
+    return line
 
 def get_bool(line):
-    pass
+    __config = __split('bool', line)
+    boolean = BooleanType()
+    for inputs in __config:
+        param = boolean.get_boolean()
+        line = line.replace('bool()', str(param))
+
+    return line
 
 def get_attrib(line, is_attribution=False):
     if not is_attribution:
@@ -67,17 +86,14 @@ def get_attrib(line, is_attribution=False):
 
         return (name, value)
 
-def get_variable(line):
-    pass
-
-def read_for(line):
-    pass
-
-def read_while(line):
+def get_seq(line):
+    # To do
     pass
 
 if __name__ == "__main__":
-    line = 'str(4, a...e) int(0<>100) str(10, a...b) float() str(3, a...b) int(5<>10)'
+    line = 'str(4, a...e) bool() int(0<>100) str(10, a...b) float(3<>5, 2) str(3, a...b) int(5<>10)'
     line = get_str(line)
     line = get_int(line)
+    line = get_float(line)
+    line = get_bool(line)
     print(line)
