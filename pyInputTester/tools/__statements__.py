28373203deabead7bbd23923b6e6942dc9a4996e
davig-sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2019-11-29 17:03:26 -0300

    Added Seq Statement

--- pyInputTester/tools/__statements__.py
+++ pyInputTester/tools/__statements__.py
@@ -7,21 +7,29 @@ com os valores, utilizando do mesmo exemplo acima, o valor retornado seria: 97 T
 50 False, etc.
 '''
 
-''' 
-Recebe como parâmetro os a string(int, str, bool, etc.) a ser procurada na linha passada,
-e retorna uma lista apenas com as especificações. No caso de inteiros '0<>100', strings
-'a...z', etc.
-'''
+
 def __split(string, line):
+    ''' 
+    Recebe como parâmetro os a string(int, str, bool, etc.) a ser procurada na linha passada,
+    e retorna uma lista apenas com as especificações. No caso de inteiros '0<>100', strings
+    'a...z', etc.
+    '''
     att = ''
     config = []
     for i in range(len(line)-len(string)-1):
         for k in range(len(string)):
-            att += line[i+k] 
+            att += line[i+k]
         if att == string:
             j = i+len(string)
+            cont = -1
             while True:
-                if line[j-1] == ')': break
+                if line[j-1] == '(':
+                    cont += 1
+                if line[j-1] == ')':
+                    if cont == 0:
+                        break
+                    else:
+                        cont -= 1
                 att += line[j]
                 j += 1
             config.append(att)
@@ -32,12 +40,13 @@ def __split(string, line):
         config = [att.strip(')') for att in config]
     return config
 
-'''
-Recebe a lista com as configurações das entradas
-e faz os respectivos splits necessários para cada uma delas,
-de forma a deixar somente os limitadores 
-'''
+
 def get_details(_type, __config):
+    '''
+    Recebe a lista com as configurações das entradas
+    e faz os respectivos splits necessários para cada uma delas,
+    de forma a deixar somente os limitadores 
+    '''
     if _type == 'int':
         __config = [param.split('<>') for param in __config]
     elif _type == 'str':
@@ -52,61 +61,68 @@ def get_details(_type, __config):
             params[1] = params[1].strip()
     return __config
 
-'''
-Preenche a linha com números inteiros variados, de acordo com os limites
-passados.
-'''
+
 def get_int(line):
+    '''
+    Preenche a linha com números inteiros variados, de acordo com os limites
+    passados.
+    '''
     __config = __split('int', line)
     __config = get_details('int', __config)
     integer = IntegerType()
     for inputs in __config:
         param = integer.get_integer(int(inputs[0]), int(inputs[1]))
-        string = 'int(' + inputs[0] + '<>' + inputs[1] +')'
-        line = line.replace(string, str(param))
-    
+        string = 'int(' + inputs[0] + '<>' + inputs[1] + ')'
+        line = line.replace(string, str(param), 1)
+
     return line
 
-'''
-Preenche a linha com strings variadas, de acordo com os limites passados
-'''
+
 def get_str(line):
+    '''
+    Preenche a linha com strings variadas, de acordo com os limites passados
+    '''
     __config = __split('str', line)
     __config = get_details('str', __config)
     string = StringType()
     for inputs in __config:
         param = string.get_string(int(inputs[0]), inputs[1][0], inputs[1][1])
-        old_string = 'str(' + inputs[0] + ', ' + inputs[1][0] + '...' + inputs[1][1] + ')' 
-        line = line.replace(old_string, param)
+        old_string = 'str(' + inputs[0] + ', ' + \
+            inputs[1][0] + '...' + inputs[1][1] + ')'
+        line = line.replace(old_string, param, 1)
 
     return line
 
-'''
-Preenche a linha com floats variados, de acordo com os limites passados
-'''
+
 def get_float(line):
-    __config = __split('float', line) 
+    '''
+    Preenche a linha com floats variados, de acordo com os limites passados
+    '''
+    __config = __split('float', line)
     __config = get_details('float', __config)
     float_type = FloatType()
 
     for __conf in __config:
         param = float_type.get_float(__conf[0][0], __conf[0][1], __conf[1])
-        old_string = 'float(' + __conf[0][0] + '<>' + __conf[0][1] + ', ' + __conf[1] + ')'
-        line = line.replace(old_string, str(param))
-    
+        old_string = 'float(' + __conf[0][0] + '<>' + \
+            __conf[0][1] + ', ' + __conf[1] + ')'
+        line = line.replace(old_string, str(param), 1)
+
     return line
 
-'''
-Preenche a linha com booleans variados
-'''
+
 def get_bool(line):
+    '''
+    Preenche a linha com booleans variados
+    '''
     __config = __split('bool', line)
     boolean = BooleanType()
     for inputs in __config:
         param = boolean.get_boolean()
-        line = line.replace('bool()', str(param))
+        line = line.replace('bool()', str(param), 1)
 
     return line
+
 
 def get_attrib(line):
     line = line.split('$')[1]
@@ -118,12 +134,23 @@ def get_attrib(line):
 
     return (name, value)
 
+
 def get_seq(line):
-    # To do
-    pass
+    __config = __split('seq', line)
+    print(__config)
+    for params in __config:
+        item, times = [item.strip() for item in params.split(',')]
+        string = ''
+        for time in range(int(times)):
+            space = ' ' if time != int(times) - 1 else ''
+            string += item + space
+        line = line.replace('seq(' + params + ')', string, 1)
+    return line
+
 
 if __name__ == "__main__":
-    line = 'str(4, a...e) bool() int(0<>100) str(10, a...b) float(3<>5, 2) str(3, a...b) int(5<>10)'
+    line = 'str(4, a...e) seq(int(0<>10), 5) bool() int(0<>100) float(3<>5, 2) str(3, a...b) int(5<>10)'
+    line = get_seq(line)
     line = get_str(line)
     line = get_int(line)
     line = get_float(line)
