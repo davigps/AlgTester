874b65876dfb2ec9b6b773cc9ef2994643e6021c
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-21 23:16:40 -0300

    feat: add new bug

--- examples/creating_tests_cases.py
+++ examples/creating_tests_cases.py
@@ -1,14 +1,16 @@
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 from pyInputTester.tools.GeneratorInput import GeneratorInput
 
 # If you don't set the input test configuration file name, '.test' file will be set.
 # The first argument is the number of random tests cases.
-gerador = GeneratorInput(5, 'example.test')
+gerador = GeneratorInput(5, "example.test")
 
 # This method will read the input test configuration and generate random inputs.
 # A list with all the tests cases will be returned, so you can use it in Tester class.
 inputs = gerador.generate_inputs()
 
-print(inputs)
+for case in inputs:
+    print(case)
