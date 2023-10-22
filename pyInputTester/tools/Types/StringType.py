e934e1f435b6e9559d972ae90cc607fff67952c8
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-10-21 23:16:51 -0300

    feat: add new bug

--- pyInputTester/tools/Types/StringType.py
+++ pyInputTester/tools/Types/StringType.py
@@ -1,18 +1,19 @@
 import random
 import string
 
+
 class StringType:
-    
     alphabet = string.ascii_letters
 
     def __init__(self):
         pass
 
     def get_string(self, length, init, final):
-        string = ''
+        string = ""
         init = self.alphabet.index(init)
         final = self.alphabet.index(final)
         for letter in range(length):
-            string += self.alphabet[random.randint(init, final)]
+            random_index = random.randint(init, final)
+            string += self.alphabet[init]
 
         return string
